d0281dfeca8a2de62cb8f13690546e11602a177f
Vladislav Volkov <ortogorbd@gmail.com>
2018-11-02 01:51:51 +0200

    Таблица данных

---
+++ CuttingForceMeasurement/SensorDataItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuttingForceMeasurement
+{
+    public class SensorDataItem
+    {
+        // цикл 50 мидисек
+        // милисекунды
+        public double Time { get; set; }
+        // Ускорение м/с^2
+        public double Acceleration { get; set; }
+        // Усилие кН
+        public double Force { get; set; }
+        // Напряжение, В
+        public double Voltage { get; set; }
+        // Ток, А
+        public double Amperage { get; set; }
+        // Частота оборотов, об/микросек
+        public double Rpm { get; set; }
+    }
+}
